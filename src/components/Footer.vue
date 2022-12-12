7b75c559c34c3b102cb010814aad0f4a7cd4ca8d
Lazola2 <lazolamakubalo271@gmail.com>
2022-12-12 16:38:18 +0200

    jfs

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer>
+        <h2>This is the footer</h2>
+    </footer>
+</template>
+<script>
+    export default {name: 'FooterPanel'}
+</script>
---
+++ src/views/Contacts.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>This is the contact form</h1>
+</template>
+
+<style scoped>
+   
+</style>
